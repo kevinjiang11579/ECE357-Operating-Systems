da96696fd8586e2017e9d53cf55ce36f057242cd
kevinjiang11579 <kevinjiang11579@gmail.com>
2020-09-15 14:21:48 -0400

    More improvements

--- kitty.c
+++ kitty.c
@@ -11,13 +11,10 @@ int main(int argc, char *argv[])
 	char buf[4096];
 	int lim, readResult, writeResult, getoptResult, fdIn, fdOut, closeResult, bytesWritten, countRW;
 	lim = sizeof(buf);
-	char *outName = NULL;
-	char *inName = NULL;
-	char *currentArg = NULL;
+	char *outName = NULL, *inName = "<standard input>", *currentArg = NULL;
 	extern char *optarg;
 	extern int optind, opterr, optopt;
-	_Bool outExist = 0;
-	_Bool isBinary = 0;
+	_Bool outExist = 0, isBinary = 0;
 	fdOut = STDOUT_FILENO;
 	fdIn = STDIN_FILENO;
 	while((getoptResult = getopt(argc, argv, "o:")) != -1)
@@ -31,30 +28,26 @@ int main(int argc, char *argv[])
 					return -1;
 				}
 				outName = optarg;
-				printf("Output file name is %s\n", outName);
 				fdOut = open(outName, O_WRONLY|O_CREAT|O_APPEND, 0666);
 				outExist = 1;
 				break;
 		}
 	}
-	if(optind == 1)
-	{
-		printf("No output file specified, printing to stdout\n");
-	}
 	if(optind == argc)
 	{
-		printf("No input file specifed, reading from stdin\n");
-		readResult = read(fdIn, buf, lim);
-		if(readResult < 0)
+		while((readResult = read(fdIn, buf, lim)) != 0);
 		{
-			printf("Error occured when reading file, errno: %s\n", strerror(errno));
-			return -1;
-		}
-		else
-		{
-			printf("%d bytes read\n", readResult);
-			writeResult = write(fdOut, buf, readResult);
-			printf("%d bytes written\n", writeResult);
+			if(readResult < 0)
+			{
+				printf("Error occured when reading from %s, errno = %d, %s\n", inName, errno, strerror(errno));
+				return -1;
+			}
+			else
+			{
+				writeResult = write(fdOut, buf, readResult);
+				bytesWritten += writeResult;
+				countRW += 1;
+			}
 		}
 	}
 	else
@@ -64,13 +57,12 @@ int main(int argc, char *argv[])
 			currentArg = argv[argIndex];
 			if(currentArg[0] == '-')
 			{
-				printf("Reading input from stdin\n");
 				fdIn = STDIN_FILENO;
+				inName = "<standard input>";
 			}
 			else
 			{
 				inName = currentArg;
-				printf("Input file name is: %s\n", inName);
 				fdIn = open(inName, O_RDONLY);
 			}
 			while((readResult = read(fdIn, buf, lim)) != 0)
@@ -83,30 +75,23 @@ int main(int argc, char *argv[])
 				}
 				if(readResult < 0)
 				{
-					printf("Error occured when reading file, errno: %s\n",strerror(errno));
+					printf("Error occured when reading %s, errno = %d, %s\n", inName, errno, strerror(errno));
 					return -1;
 				}
-				else
-				{
-					printf("%d bytes read\n", readResult);
-				}
-			writeResult = write(fdOut, buf, readResult);
-			bytesWritten += writeResult;
-			countRW += 1;
+				writeResult = write(fdOut, buf, readResult);
+				bytesWritten += writeResult;
+				countRW += 1;
 			}
-			printf("End of file reached\n");
 			if(isBinary){printf("WARNING: input file is a binary file\n");}
-			if(inName == STDIN_FILENO){inName = "<standard input>";};
 			printf("Input file: %s, Total bytes written: %d, Read/Writes made: %d\n", inName, bytesWritten, countRW);
 			if(fdIn > 0)
 			{
 				closeResult = close(fdIn);
 				if(closeResult != 0)
 				{
-					printf("Error occured when closing file, errno: %s\n", strerror(errno));
+					printf("Error occured when closing %s, errno = %d, %s\n", inName, errno, strerror(errno));
 					return -1;
 				}
-				else{printf("File successfully closed\n");}
 			}
 		}
 	}
