ba43b9fa7c3ab3101e260d3323c1e68ec14baf13
Kevin Jiang <jiang9@cooper.edu>
2020-09-14 20:58:27 -0400

    Initial test compile

--- kitty.c
+++ kitty.c
@@ -1,6 +1,11 @@
 /*ECE357 Assignment 1, Kevin Jiang*/
-#include <stdio>
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc == 1)
+	{
+		printf("Reading from stdin, printing to stdout\n");
+	}
+	return 0;
 }
