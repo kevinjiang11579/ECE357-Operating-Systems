dfb236273dc421e63de9b6c6e129571395f2052c
kevinjiang11579 <kevinjiang11579@gmail.com>
2020-09-14 22:15:30 -0400

    Added cases

--- kitty.c
+++ kitty.c
@@ -1,19 +1,22 @@
 /*ECE357 Assignment 1, Kevin Jiang*/
 #include <stdio.h>
 #include <unistd.h>
+#include <getopt.h>
 int main(int argc, char *argv[])
 {
 	char buf[4096];
-	int lim;
-	int readResult;
-	int writeResult;
+	int lim, readResult, writeResult, getoptResult;
 	lim = sizeof(buf);
+	char *outName = NULL;
+	extern char *optarg;
+	extern int optind, opterr, optopt;
 	if(argc == 1)
 	{
 		printf("Reading from stdin, printing to stdout\n");
 		readResult = read(STDIN_FILENO, buf, lim);
 		if(readResult == 0)
 		{
+			printf("End of file reached\n");
 		}
 		else if(readResult < 0)
 		{
@@ -30,6 +33,17 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		while((getoptResult = getopt(argc, argv, "o:")) != -1)
+		{
+			switch (getoptResult)
+			{
+				case 'o':
+					outName = optarg;
+					printf("Output file name is %s", outName);
+					break;
+			}
+		}
+		printf("optind debug: %d", optind);
 	}
 	return 0;
 }
